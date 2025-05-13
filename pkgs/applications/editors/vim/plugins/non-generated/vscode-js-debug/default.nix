b6f9611a4982ab40441b7d5f71241e66cd51da49
Relief Melone <relief.melone@gmail.com>
2025-05-13 12:40:15 +0200

    removing package-lock.json

--- pkgs/applications/editors/vim/plugins/non-generated/vscode-js-debug/default.nix
+++ pkgs/applications/editors/vim/plugins/non-generated/vscode-js-debug/default.nix
@@ -34,7 +34,7 @@ let
       jq '.dependencies += { "merge2": "^1.4.1", "vsce":"^2.7.0", "@dprint/linux-x64-glibc": "^0.49.1", "@esbuild/linux-x64": "0.25.4" }' package.json > tmp-package.json
       mv tmp-package.json package.json
 
-      npm i --package-lock-only
+      rm package-lock.json
 
       mkdir $out
       cp -r ./* $out
