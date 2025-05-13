c9c775cad50c9b830ddfc829e1c2a2f15c13d64a
Relief Melone <relief.melone@gmail.com>
2025-05-13 23:08:17 +0200

    using buildPhase instead of installPhase

--- pkgs/applications/editors/vim/plugins/non-generated/vscode-js-debug/default.nix
+++ pkgs/applications/editors/vim/plugins/non-generated/vscode-js-debug/default.nix
@@ -26,7 +26,7 @@ let
 
     buildInputs = with pkgs; [ jq nodejs];
 
-    installPhase = ''
+    buildPhase = ''
       echo "adding dependencies"
       jq '.dependencies += { "picomatch": "^4.0.2", "@esbuild/linux-x64-glibc": "^0.49.1" }' package.json > package-temp.json
       mv package-temp.json package.json
