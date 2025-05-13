6601414a61fa56ed109bc3fe413d45545b1fcebc
Relief Melone <relief.melone@gmail.com>
2025-05-13 11:06:14 +0200

    omit=dev

--- pkgs/applications/editors/vim/plugins/non-generated/vscode-js-debug/default.nix
+++ pkgs/applications/editors/vim/plugins/non-generated/vscode-js-debug/default.nix
@@ -24,7 +24,7 @@ let
 
     npmDepsHash = "sha256-4SweyCohiTAMhGFwqmtQtmyic3/34azMTou6vpM2Bqo=";
     npmPackFlags = [ "--ignore-scripts" "--legacy-peer-deps" ];
-    npmInstallFlags = [ "--legacy-peer-deps" "--production" ];
+    npmInstallFlags = [ "--legacy-peer-deps" "--omit=dev" ];
     makeCacheWritable = true;
 
     nativeBuildInputs = with pkgs; [
