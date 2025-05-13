ae1de1cf41e0324b65e114188192750644b7adc4
Relief Melone <relief.melone@gmail.com>
2025-05-13 12:13:02 +0200

    adding patchPhase

--- pkgs/applications/editors/vim/plugins/non-generated/vscode-js-debug/default.nix
+++ pkgs/applications/editors/vim/plugins/non-generated/vscode-js-debug/default.nix
@@ -47,11 +47,15 @@ let
 
     NODE_OPTIONS = "--openssl-legacy-provider";
 
-    buildPhase = ''
+    patchPhase = ''
       echo "Running modifications on package.json scripts"
       jq 'del(.scripts.prepare) | del(.scripts.postinstall)' package.json > tmp-package.json
       mv tmp-package.json package.json
+
+      jq '.dependencies += { "merge2": "^1.4.1", "vsce":"^2.7.0", "@dprint/linux-x64-glibc": "^0.49.1", @esbuild/linux-x64": "0.25.4" }' package.json > tmp-package.json
+      mv tmp-package.json package.json
     '';
+
   });
 
   # def = import ./dependencies/default.nix;
