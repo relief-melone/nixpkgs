487e64f07ec5a61e626ee49748b85232a34de7e8
Relief Melone <relief.melone@gmail.com>
2025-05-13 12:14:35 +0200

    fixed typo

--- pkgs/applications/editors/vim/plugins/non-generated/vscode-js-debug/default.nix
+++ pkgs/applications/editors/vim/plugins/non-generated/vscode-js-debug/default.nix
@@ -52,7 +52,7 @@ let
       jq 'del(.scripts.prepare) | del(.scripts.postinstall)' package.json > tmp-package.json
       mv tmp-package.json package.json
 
-      jq '.dependencies += { "merge2": "^1.4.1", "vsce":"^2.7.0", "@dprint/linux-x64-glibc": "^0.49.1", @esbuild/linux-x64": "0.25.4" }' package.json > tmp-package.json
+      jq '.dependencies += { "merge2": "^1.4.1", "vsce":"^2.7.0", "@dprint/linux-x64-glibc": "^0.49.1", "@esbuild/linux-x64": "0.25.4" }' package.json > tmp-package.json
       mv tmp-package.json package.json
     '';
 
