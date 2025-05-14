18d97529dcd325d86888f8fae980dfa04b42fbab
Relief Melone <relief.melone@gmail.com>
2025-05-14 20:58:11 +0200

    no dprint linux for testing

--- pkgs/applications/editors/vim/plugins/non-generated/vscode-js-debug/default.nix
+++ pkgs/applications/editors/vim/plugins/non-generated/vscode-js-debug/default.nix
@@ -33,7 +33,7 @@ let
       export npm_config_strict_ssl="false"
 
       echo "adding dependencies"
-      jq '.dependencies += { "picomatch": "^4.0.2", "@esbuild/linux-x64": "0.25.3", "@dprint/linux-x64-glibc": "0.49.1" }' package.json > package-temp.json
+      jq '.dependencies += { "picomatch": "^4.0.2", "@esbuild/linux-x64": "0.25.3" }' package.json > package-temp.json
       mv package-temp.json package.json
 
       echo "removing scripts"
