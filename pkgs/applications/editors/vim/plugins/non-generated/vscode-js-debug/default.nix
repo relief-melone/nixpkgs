bed197db86559fdcf8baa4f61f194d22a87824ca
Relief Melone <relief.melone@gmail.com>
2025-05-14 14:19:03 +0200

    added env-vars

--- pkgs/applications/editors/vim/plugins/non-generated/vscode-js-debug/default.nix
+++ pkgs/applications/editors/vim/plugins/non-generated/vscode-js-debug/default.nix
@@ -27,6 +27,10 @@ let
     buildInputs = with pkgs; [ jq nodejs];
 
     buildPhase = ''
+      export NIX_STORE_DIR=$HOME/nix/store
+      export NIX_STATE_DIR=$HOME/nix/var/nix
+      export NIX_LOG_DIR=$HOME/nix/var/log/nix
+
       echo "adding dependencies"
       jq '.dependencies += { "picomatch": "^4.0.2", "@esbuild/linux-x64-glibc": "^0.49.1" }' package.json > package-temp.json
       mv package-temp.json package.json
