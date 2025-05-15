922b0f2067fe18077f2cdff145ca38f06d2fa6ca
Relief Melone <relief.melone@gmail.com>
2025-05-15 14:13:25 +0200

    no npmDepsHash

--- pkgs/applications/editors/vim/plugins/non-generated/vscode-js-debug/default.nix
+++ pkgs/applications/editors/vim/plugins/non-generated/vscode-js-debug/default.nix
@@ -86,7 +86,6 @@ let
     npmPackFlags = [ "--ignore-scripts" "--legacy-peer-deps" ];
     npmInstallFlags = [ "--legacy-peer-deps" "--ignore-scripts" ];
     npmFlags = [ "--ignore-scripts" "--legacy-peer-deps" ];
-    npmDepsHash = "sha256-4SweyCohiTAMhGFwqmtQtmyic3/34azMTou6vpM2Bqo=";
 
     npmDeps = importNpmLock {
       npmRoot = "${patch}/";
